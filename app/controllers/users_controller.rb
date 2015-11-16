5f9184a5c2956f6d76490fba7ee0ff46dc13cf74
Dan Solo <dan@solocreative.com.au>
2015-11-17 07:52:58 +1100

    add edit to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def show
+    @user = User.find(params[:id])
+    @user
 
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:success] = "User was succesfully edited"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
   def create
       @user = User.new(user_params)
       if @user.save
