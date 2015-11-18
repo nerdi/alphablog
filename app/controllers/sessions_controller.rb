79b84c0492580464e5a20580c052a6ec41e9e226
Dan Solo <dan@solocreative.com.au>
2015-11-18 19:46:31 +1100

    Complete auth suystem,

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Succesfully logged in as user"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Your password or email was incorrect"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -6,14 +8,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def edit
-    @user = User.find(params[:id])
   end
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     if @user.save
       flash[:success] = "User was succesfully edited"
@@ -37,5 +36,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
 
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def require_same_user
+    if !logged_in? || current_user != @user
+      flash[:danger] = "You can only edit or delete your own user account"
+      redirect_to root_path
+    end
   end
 end
