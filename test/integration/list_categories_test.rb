458c5c3480fa833560cbca7fb6a0c3a14818eb4f
Dan Solo <dan@solocreative.com.au>
2015-11-23 08:12:38 +1100

    Fixed List Categories Test

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: "books")
+    @category = Category.create(name: "sports")
     @category2 = Category.create(name: "programming")
   end
 
@@ -10,6 +10,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     get categories_path
     assert_template 'categories/index'
     assert_select 'a[href=?]', category_path(@category), text: @category.name
-    assert_select 'a[href=?]', category_path(@category2), text: @category.name
+    assert_select 'a[href=?]', category_path(@category2), text: @category2.name
   end
 end
