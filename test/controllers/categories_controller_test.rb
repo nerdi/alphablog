dbdcafce2534d60d8749d0365fa8982fd2d9233f
Dan Solo <dan@solocreative.com.au>
2015-11-24 07:58:54 +1100

    Add category to nav

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "Sports")
+    @admin_user = User.create(username: "Dan", email: "dan@dan.com", password: "password", admin: true)
   end
 
   test "Should get categories index" do
@@ -10,6 +11,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "Should get new" do
+    session[:user_id] = @admin_user.id
     get :new
     assert_response :success
   end
@@ -17,5 +19,11 @@ class CategoriesControllerTest < ActionController::TestCase
   test "Should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
   end
 end
